6f40067b071377b5cf8afe959c1b1bceeb8297a8
no80dy <no8odyas@gmail.com>
2024-01-13 09:10:46 +0300

    Fix some issues in main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def get_message(message: str) -> str:
     This function returns the message.
 
     parameter:
-	    - message (str): The text of message.
+        - message (str): The text of message.
 
     Returns:
-	    - str: The string message.
+        - str: The string message.
     """
     return message
 
 
 if __name__ == '__main__':
-    result = get_message('Hello world')
-    logging.info(f'RESULT: {result}')
+    message_from_stdout = get_message('Hello world')
+    logging.info(f'RESULT: {message_from_stdout}')
